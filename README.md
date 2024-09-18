7c93cf9ef8e01f544ab3f4d57f9d4188c21fd354
Viktor <komvik.slack@gmail.com>
2024-09-18 15:20:02 +0200

    Start 4_2_3_TS_Flutter_UI_Implementierung

---
+++ README.md
@@ -0,0 +1,3 @@
+# exercise_4_2_3_ts_flutter_ui_implementierung
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/exercise_4_2_3_ts_flutter_ui_implementierung/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.exercise_4_2_3_ts_flutter_ui_implementierung
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
